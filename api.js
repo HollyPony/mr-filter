90b3a230c62c56b7cc7ace2d84d0dc9b5fc6ffb5
Arthur Agombart <arthur.agombart@gmail.com>
2017-08-30 16:36:56 +0200

    feat: Add generic fetchAllPages

--- api.js
+++ api.js
@@ -16,16 +16,28 @@ module.exports = {
       .then(res => ({left: res.approvals_left}))
   },
 
-  listMergeRequests: () => {
-    return fetch(`${gitlabUrl}/api/v4/merge_requests?private_token=${gitlabKey}&state=opened&scope=all`)
-      .then(res => res.json())
-      .then(mergeRequests => Promise.all(Object.values(mergeRequests.reduce((accumulator, mergeRequest) => {
-        if (!accumulator[mergeRequest.project_id]) {
-          accumulator[mergeRequest.project_id] = {id: mergeRequest.project_id, mergeRequests: []}
-        }
+  fetchAllPages: function (url, page = 1) {
+    return fetch(`${url}&page=${page}`)
+      .then(res => {
+        const nextPage = res.headers.get('x-next-page')
+        return (nextPage && nextPage !== '')
+          ? this.fetchAllPages(url, nextPage).then(nextRes => [res].concat(nextRes))
+          : [res]
+      })
+  },
 
-        accumulator[mergeRequest.project_id].mergeRequests.push(Object.assign(mergeRequest, {approvalsLeft: 0}))
-        return accumulator
-      }, {}))))
+  listMergeRequests: function () {
+    return this.fetchAllPages(`${gitlabUrl}/api/v4/merge_requests?private_token=${gitlabKey}&state=opened&scope=all`)
+      .then(ress => Promise.all(ress.reduce((acc, res) => acc.concat(res.json()), []))) // Convert page responses to json responses
+      .then(mergeRequests => Promise.all(Object.values(mergeRequests
+        .reduce((acc, res) => acc.concat(res), []) // Flat mergeRequests
+        .reduce((accumulator, mergeRequest) => {
+          if (!accumulator[mergeRequest.project_id]) {
+            accumulator[mergeRequest.project_id] = {id: mergeRequest.project_id, mergeRequests: []}
+          }
+
+          accumulator[mergeRequest.project_id].mergeRequests.push(Object.assign(mergeRequest, {approvalsLeft: 0}))
+          return accumulator
+        }, {}))))
   }
 }
