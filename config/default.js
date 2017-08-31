0435fd7b4a001c3331933e7915aa8673b72e9071
Arthur Agombart <arthur.agombart@gmail.com>
2017-08-31 17:27:00 +0200

    chore: Fallback to oldway due to oauth shits

--- config/default.js
+++ config/default.js
@@ -1,16 +1,5 @@
 module.exports = {
   serverPort: process.env.GMR_SERVER_PORT || 3000,
-  gitlab: {
-    userKey: process.env.GITLAB_KEY,
-    url: process.env.GITLAB_URL || 'https://gitlab.com/',
-    oauth: {
-      client: {
-        id: process.env.GITLAB_APP_ID || '',
-        secret: process.env.GITLAB_SECRET_KEY || ''
-      },
-      auth: {
-        tokenHost: process.env.GITLAB_URL || 'https://gitlab.com/'
-      }
-    }
-  }
+  gitlabUrl: process.env.GITLAB_URL || '\'https://gitlab.com/\'',
+  gitlabKey: process.env.GITLAB_KEY
 }
