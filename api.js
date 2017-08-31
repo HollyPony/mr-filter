392a861e695ba538ff4bc0bb97e549c0872254fc
Arthur Agombart <arthur.agombart@gmail.com>
2017-08-31 14:59:59 +0200

    refacto: Move fetch call in a method

--- api.js
+++ api.js
@@ -9,32 +9,33 @@ module.exports = {
     params = [`?private_token=${gitlabKey}`].concat(Object.entries(params).map(([key, value]) => {
       return `${key}=${value}`
     })).join('&')
-    return `${gitlabUrl}/api/v4/${path}${params}`
+    return fetch(`${gitlabUrl}/api/v4/${path}${params}`)
   },
 
   getProjectById: function (projectId) {
-    return fetch(this.callApi(`/projects/${projectId}`))
+    return this.callApi(`/projects/${projectId}`)
       .then(res => res.json())
   },
 
   getApprovals: function (projectId, mergeRequestIid) {
-    return fetch(this.callApi(`projects/${projectId}/merge_requests/${mergeRequestIid}/approvals`))
+    return this.callApi(`projects/${projectId}/merge_requests/${mergeRequestIid}/approvals`)
       .then(res => res.json())
       .then(res => ({left: res.approvals_left}))
   },
 
-  fetchAllPages: function (url, page = 1) {
-    return fetch(`${url}&page=${page}`)
+  fetchAllPages: function (url, params) {
+    params.page = params.page || 1
+    return this.callApi(url, params)
       .then(res => {
-        const nextPage = res.headers.get('x-next-page')
-        return (nextPage && nextPage !== '')
-          ? this.fetchAllPages(url, nextPage).then(nextRes => [res].concat(nextRes))
+        params.page = res.headers.get('x-next-page')
+        return (params.page !== '')
+          ? this.fetchAllPages(url, params).then(nextRes => [res].concat(nextRes))
           : [res]
       })
   },
 
   listMergeRequests: function () {
-    return this.fetchAllPages(this.callApi('merge_requests', {state: 'opened', scope: 'all'}))
+    return this.fetchAllPages('merge_requests', {state: 'opened', scope: 'all'})
       .then(ress => Promise.all(ress.map(res => res.json()))) // Convert page responses to json responses
       .then(mergeRequests => mergeRequests.reduce((acc, res) => acc.concat(res), [])) // Flat mergeRequests
       .then(mergeRequests => mergeRequests.reduce((accumulator, mergeRequest) => { // Group mergeRequests by project
